96c77f4a249d700b2176fca8f481fbefaa202b97
Abdelrahman <abdelrahman@picassos.xyz>
2023-02-09 21:29:09 +0200

    Remove CustomTab Subscription

--- app/src/main/java/com/picassos/betamax/android/presentation/app/subscription/subscribe/SubscribeActivity.kt
+++ app/src/main/java/com/picassos/betamax/android/presentation/app/subscription/subscribe/SubscribeActivity.kt
@@ -1,20 +1,14 @@
 package com.picassos.betamax.android.presentation.app.subscription.subscribe
 
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.browser.customtabs.CustomTabColorSchemeParams
-import androidx.browser.customtabs.CustomTabsIntent
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.picassos.betamax.android.BuildConfig
 import com.picassos.betamax.android.R
 import com.picassos.betamax.android.core.utilities.Helper
-import com.picassos.betamax.android.core.utilities.Helper.isPackageInstalled
 import com.picassos.betamax.android.databinding.ActivitySubscribeBinding
 import com.picassos.betamax.android.di.AppEntryPoint
 import dagger.hilt.android.AndroidEntryPoint
@@ -72,22 +66,7 @@ class SubscribeActivity : AppCompatActivity() {
                             diamondPackage.background = getDrawable(R.drawable.pricing_background_selected)
                         }
                         subscribe.setOnClickListener {
-                            val params = CustomTabColorSchemeParams.Builder().apply {
-                                setToolbarColor(ContextCompat.getColor(this@SubscribeActivity, R.color.color_dark))
-                            }
-                            val builder = CustomTabsIntent.Builder().apply {
-                                setDefaultColorSchemeParams(params.build())
-                                setShowTitle(true)
-                                setShareState(CustomTabsIntent.SHARE_STATE_OFF)
-                                setInstantAppsEnabled(false)
-                                setUrlBarHidingEnabled(true)
-                            }.build()
 
-                            if (this@SubscribeActivity.isPackageInstalled("com.android.chrome")) {
-                                builder.intent.setPackage("com.android.chrome")
-                                builder.launchUrl(this@SubscribeActivity, Uri.parse(
-                                    "${BuildConfig.BASE_URL}subscription/subscribe.php?paymentToken=${account.paymentToken}&package=${selectedPackage}"))
-                            }
                         }
                     }
                 }.collect()
