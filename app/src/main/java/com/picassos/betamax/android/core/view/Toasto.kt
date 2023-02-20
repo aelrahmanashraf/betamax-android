ad8d7dc73168cdb0a9a9111d6d7a3bd8aa4944a0
Abdelrahman <abdelrahman@picassos.xyz>
2023-02-20 03:49:19 +0200

    Update toast and fix some issues

--- app/src/main/java/com/picassos/betamax/android/core/view/Toasto.kt
+++ app/src/main/java/com/picassos/betamax/android/core/view/Toasto.kt
@@ -8,54 +8,34 @@ import android.view.View
 import com.picassos.betamax.android.R
 import android.widget.TextView
 import android.widget.Toast
-import java.util.*
 
 object Toasto {
-    /**
-     * use @show_toast to show a custom toast with
-     * text, duration and style. for example:
-     * show_toast(context, title, duration, type);
-     * we assigned null to the layout inflater instead
-     * of assigning @custom_taast to root as it will always
-     * return null. Toast design is designed to be clean.
-     * @param context for context
-     * @param title for toast title
-     * @param duration for duration, you can whether
-     * choose 0 for short period or 1 for long period
-     * @param type for toast type. currently support designs
-     * is (0) #primary, (1) #alert, (2) #warning
-     */
-    @JvmStatic
     @SuppressLint("InflateParams")
-    fun showToast(context: Context, title: String?, duration: Int, type: Int) {
-        // layout inflater
+    fun showToast(context: Context, title: String, duration: Int, type: Int) {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        var layout: View? = null
-        when (type) {
+        val layout: View? = when (type) {
             0 -> {
-                layout = Objects.requireNonNull(inflater).inflate(R.layout.toast_layout_primary, null)
+                inflater.inflate(R.layout.toast_layout_primary, null)
             }
             1 -> {
-                layout = Objects.requireNonNull(inflater).inflate(R.layout.toast_layout_alert, null)
+                inflater.inflate(R.layout.toast_layout_alert, null)
             }
             2 -> {
-                layout = Objects.requireNonNull(inflater).inflate(R.layout.toast_layout_warning, null)
+                inflater.inflate(R.layout.toast_layout_warning, null)
+            }
+            else -> null
+        }
+        layout?.let { view ->
+            view.findViewById<TextView>(R.id.toast_text)?.apply {
+                text = title
+            }
+
+            Toast(context).apply {
+                setGravity(Gravity.TOP or Gravity.FILL_HORIZONTAL, 0, 0)
+                this.duration = duration
+                this.view = layout
+                show()
             }
         }
-
-        // initialize toast text and set text from the method
-        val toastTitle = layout?.findViewById<TextView>(R.id.toast_text)
-        if (toastTitle != null) {
-            toastTitle.text = title
-        }
-
-        // create a new toast and set gravity as @fill_horizontal
-        // and @gravity.top to make the toast stick on the top.
-        val toast = Toast(context)
-        toast.setGravity(Gravity.TOP or Gravity.FILL_HORIZONTAL, 0, 0)
-        toast.duration = duration
-        toast.view = layout
-        // show the toast
-        toast.show()
     }
 }
